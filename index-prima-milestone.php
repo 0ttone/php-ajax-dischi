9fe47af63579eb93fa9850214f1d82de51f916d3
MyUsername <emanuele.marini37@gmail.com>
2021-08-30 17:05:59 +0200

    cambio sintassi

--- index-prima-milestone.php
+++ index-prima-milestone.php
@@ -4,21 +4,31 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+      <link rel="stylesheet" href="./css/style.css">
       <title>Index-prima</title>
 </head>
 <body>
       <?php
-      require_once__DIR__ . "/database/database.php";
 
-      foreach($database as $disc) {
-            echo "{$disc['title']} <br/>";
-            echo "{$disc['author']} <br/>";
-      }
+      require_once__DIR__ . "/database/database.php";
+      ?>
+
+     <?php var_dump($database);?>
+
+      <?php
+      foreach($database as $disc) {?>
+
+            <div class=>
+                  <img src="<?php echo $disc['poster']?>" alt="<?php echo "cover album - {$disc['title']}"?>"
+                   <h5><?php echo $disc['title'] ?></h5>
+                   <h5><?php echo $disc['author'] ?></h5>
+                   <h5><?php echo $disc['year'] ?></h5>
+            </div>
+      
 
 
       
-      ?>
+      
 
 
 
